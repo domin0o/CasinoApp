0131a202c5fb5d1b69066b5753b8ed5e0df95cee
Dominik Skrzydlewski <skrzydlewskijunior@gmail.com>
2023-01-14 22:02:18 +0100

    Dodaj pliki projektów.

---
+++ CasinoApp/Guy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoApp
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+        /// <summary>
+        /// Wyświetla w konsoli imię i posiadaną kwotę
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " ma " + Cash + " zł.");
+        }
+        /// <summary>
+        /// Przekazuje pieniądze i usuwa je z portfela (lub wyświetla w konsoli komunikat o braku środków).
+        /// </summary>
+        /// <param name="amount">Przekazywana kwota</param>
+        /// <returns>
+        /// Ilość pieniędzy wyjmowanych z portfela lub 0, jeśli dostępne środki są za małe (lub parametr jest nieprawidłowy)
+        /// </returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " mówi: " + amount + " nie jest poprawną kwotą.");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " mówi:" + " nie mam wystarczających środków, aby dać ci " + amount + " zł.");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " mówi: " + "nie przyjmuję " + amount + " zł.");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
---
+++ CasinoApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.75;
+            Guy player = new Guy() { Name = "Gracz", Cash = 100 };
+
+            Console.WriteLine("Witaj w kasytnie. Prawdopodobieństwo przegranej: 0.75");
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+
+                Console.Write("Stawiana kwota: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int pot))
+                {
+                    if (pot > 0)
+                    {
+                        pot = player.GiveCash(pot) * 2;
+                        if (random.NextDouble() > odds)
+                        {
+                            int winning = pot;
+                            player.ReceiveCash(winning);
+                            Console.WriteLine("Wygrałeś " + winning + " zł.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Niestety, przegrałeś.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Musisz postawić kwotę większą niż 0.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Musisz wpisać liczbę!");
+                }
+            }
+            Console.WriteLine("Kasyno zawsze wygrywa.");
+            Console.ReadLine();
+        }
+    }
+}
